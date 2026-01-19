20d046210de06d7bfbdfa302f1a5752b3950f872
KpCeg4 <kpceg4@gmail.com>
2026-01-19 13:43:47 +0530

    console log debug axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+
+console.log("API BASE URL =", process.env.REACT_APP_API_BASE_URL);
+
+
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
   headers: {
