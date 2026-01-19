aa794a9373cc00c15492b696c74eb89205c52b7b
KpCeg4 <kpceg4@gmail.com>
2026-01-19 13:57:29 +0530

    temp hardcode api base url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
-
-console.log("API BASE URL =", process.env.REACT_APP_API_BASE_URL);
-
-
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: "https://online-bookstore-backend-g4c4.onrender.com",
   headers: {
     "Content-Type": "application/json"
   }
